d76752ee1537aecef11a679a493eab8dc1f25903
faheem209 <faheem.uosbk@gmail.com>
2025-08-13 01:14:47 -0700

    Files Created
    New files created and updated

---
+++ script.js
@@ -0,0 +1,8 @@
+//alert("Hello World");
+function add(){
+var a = prompt("Enter a number");
+var b = prompt("Enter a number");
+    if(a && b){
+        alert("The sum of the two numbers is " + (a + b));
+    }
+}
